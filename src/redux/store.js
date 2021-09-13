b6bc7b8facb918f0e9a72805cb46aaf88888c734
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2021-09-13 22:22:58 +0100

    setup redux store

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import covidReducer from './covid/covid';
+
+const reducers = combineReducers({
+  covid: covidReducer,
+});
+
+const store = createStore(
+  reducers,
+  applyMiddleware(ReduxThunk),
+);
+
+export default store;
